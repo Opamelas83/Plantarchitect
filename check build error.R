8daa5d9b717cfd480f3fa952ed97b03d041e70a5
Opamelas83 <omichellepamelas@gmail.com>
2024-10-19 18:24:19 -0500

    Octo_2024

---
+++ check build error.R
@@ -0,0 +1,2 @@
+cat(readLines("/private/var/folders/77/1qm_6c_10d35cq9jywtzbv5www6yj8/T/Rtmp6A6P9O/workflowr/data_curation.Rmd-2023-03-01-12h-58m-48s-out.txt"), sep = "\n")
+cat(readLines("/private/var/folders/77/1qm_6c_10d35cq9jywtzbv5www6yj8/T/Rtmp6A6P9O/workflowr/fieldtrialanalysis.Rmd-2023-03-01-13h-10m-47s-err.txt"), sep = "\n")
